006850b48dc09e6421462b93d03030e008b7d136
Ryan H. Lewis <me@ryanlewis.net>
2015-01-31 22:25:07 -0800

    i think it works, but homebrew is so hipster.

--- ctlib.rb
+++ ctlib.rb
@@ -2,19 +2,23 @@ require "formula"
 
 class Ctlib < Formula
   homepage "http://ctl.appliedtopology.org"
-  url "http://ctl.appliedtopology.org/r/v0.1.tgz"
-  sha1 "9c8ad348c1760a3646cc454b1e9cb66b695d58f6"
+  url "https://github.com/appliedtopology/ctl/archive/v0.2.tar.gz"
+  sha1 "2cc4f0f8c31e4943529d99570025b2424630ec5e"
 
   depends_on "cmake" => :build
   depends_on "tbb"
   depends_on "boost"
   depends_on "metis"
+  depends_on "doxygen"
+  depends_on "openmpi"
+  depends_on "ann"
 
   def install
      args = std_cmake_args
      args << "-DCXXFLAGS=-mmacosx-version-min"
      system "cmake", *args
-     system "make VERBOSE=1"
+     system "make -j VERBOSE=1"
+     system "make doc"
      system "make install"
   end
 end
